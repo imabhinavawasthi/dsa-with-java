6354aeeb2a930c3b98e967203cb5f9e433d9bdda
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2026-05-09 12:21:00 +0530

    OOPs

---
+++ SessionOOPs/Car.java
@@ -0,0 +1,21 @@
+package SessionOOPs;
+
+public class Car {
+    String brand;
+    int price;
+    String color;
+    Car(String brand, int price, String color) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    }
+    public void printCarDetails() {
+        System.out.println("Brand: " + this.brand);
+    }
+    public static void main(String[] args) {
+        Car car1 = new Car("Mahindra", 1000000, "Black");
+        Car car2 = new Car("Toyota", 10000000, "White");
+
+        car1.printCarDetails();
+    }
+}
